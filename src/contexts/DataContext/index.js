58f8204080ffb0451bde465bf4689b84da2b4642
boris blanche <borisblanche77gmail.com>
2024-08-01 01:06:05 +0200

    projet ok

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -51,49 +51,4 @@ DataProvider.propTypes = {
 export const useData = () => useContext(DataContext);
 
 export default DataContext;
-// import PropTypes from "prop-types";
-// import { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
 
-// const DataContext = createContext({});
-
-// export const api = {
-//   loadData: async () => {
-//     const json = await fetch("/events.json");
-//     return json.json();
-//   },
-// };
-
-// export const DataProvider = ({ children }) => {
-//   const [error, setError] = useState(null);
-//   const [data, setData] = useState(null);
-
-//   const getData = useCallback(async () => {
-//     try {
-//       setData(await api.loadData());
-//     } catch (err) {
-//       setError(err);
-//     }
-//   }, []);
-
-//   useEffect(() => {
-//     if (!data) {
-//       getData();
-//     }
-//   }, [data, getData]);
-
-//   const contextValue = useMemo(() => ({ data, error }), [data, error]);
-
-//   return (
-//     <DataContext.Provider value={contextValue}>
-//       {children}
-//     </DataContext.Provider>
-//   );
-// };
-
-// DataProvider.propTypes = {
-//   children: PropTypes.node.isRequired,
-// };
-
-// export const useData = () => useContext(DataContext);
-
-// export default DataContext;
